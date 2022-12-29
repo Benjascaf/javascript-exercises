34e240e8fa40800a6583bd1c3b7943a8ce60c495
benjascaf <benjascaf@gmail.com>
2022-12-29 18:51:28 -0300

    Listo

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(srcArray, ...toBeRemoved) {
+    for (let i = 0; i < srcArray.length; i++) {
+        while (toBeRemoved.includes(srcArray[i])) {
+            srcArray.splice(i, 1);
+        }
+    }
+    return srcArray;
 };
-
+removeFromArray([1,2,3,4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
